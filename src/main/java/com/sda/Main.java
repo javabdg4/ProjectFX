ea8c022452bc32254861d5e420fd94e809451665
Bartek Welter <welterbartek8@gmail.com>
2018-09-03 19:09:20 +0200

    add stage and window to InteliJ project

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -1,7 +1,36 @@
 package com.sda;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    private VBox vbox;
+    private Stage stage;
+
     public static void main(String[] args) {
-        
+        launch();
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        this.stage = primaryStage;
+        loadView();
+    }
+
+    public void loadView(){
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("/RootView.fxml"));
+        try {
+            vbox = (VBox) loader.load();
+            Scene scene = new Scene(vbox);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
